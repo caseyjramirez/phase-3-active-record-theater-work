c564741cc9f521cf86649d21be3575c511459149
Alvin <alvin.sanchez27@gmail.com>
2022-09-27 11:02:18 -0400

    added two methods to role.rb

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,15 @@
 class Role < ActiveRecord::Base
     has_many :auditions
+
+    def actors
+        self.auditions.map do |audition|
+            audition.actor
+        end
+    end
+
+    def locations
+        self.auditions.map do |audition|
+            audition.location
+        end
+    end
 end
